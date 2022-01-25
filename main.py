644673076497c54e7e063a47d0db40951bea02f1
pjb75 <pjb7525@gmail.com>
2022-01-25 22:01:38 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+a = "banana"
+b = "park jung bom"
+print("%s는 %s가 좋아하는 과일이다."%(a, b))
+
+print(f"{a} {b}")
+
+print("박정범", "바나나")
+print(a, b, a, b, end=' ---야옹--- ')
+print(a, b, a, b, sep=' 멍멍 ')
+'''
+#연습문제1
+'''str = input("이름을 입력하세요:")
+print("%s님 안녕하세요. "%str)
+'''
+#mission
+name = input("이름을 입력하세요.")
+say = input("지금 하고 싶은 말은?")
+print(f"{name}은 {say}라고 말했다.")
